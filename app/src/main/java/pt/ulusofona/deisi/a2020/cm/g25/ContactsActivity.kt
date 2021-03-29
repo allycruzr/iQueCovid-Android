13c9ae1820cafeeec5588f7edd8f6e5fa5831def
Rui Moreira - 21600035 <a21600035@alunos.ulht.pt>
2021-03-29 23:43:30 +0100

    First Commit

---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/ContactsActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.a2020.cm.g25
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/SplashActivity.kt
@@ -0,0 +1,11 @@
+package pt.ulusofona.deisi.a2020.cm.g25
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
