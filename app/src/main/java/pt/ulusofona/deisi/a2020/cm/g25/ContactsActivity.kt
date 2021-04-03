f49dfe36b096fbaf940ccf4f72ef06dc275bddc1
Rui Moreira - 21600035 <a21600035@alunos.ulht.pt>
2021-04-03 01:53:25 +0100

    Actualizações no código

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/ContactsActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/ContactsActivity.kt
@@ -3,8 +3,6 @@ package pt.ulusofona.deisi.a2020.cm.g25
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_contacts.*
 
