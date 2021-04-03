74c7756850c8b332abcc04199f7963fbe22391b0
Rui Moreira - 21600035 <a21600035@alunos.ulht.pt>
2021-04-03 04:25:01 +0100

    Criada TestDetailActivity.kt e bottom_navigation_menu.xml criados

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/DashboardActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/DashboardActivity.kt
@@ -3,12 +3,27 @@ package pt.ulusofona.deisi.a2020.cm.g25
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.dashboard_page -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.test_list_page -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     override fun onStart() {
---
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/TestDetailActivity.kt
@@ -0,0 +1,16 @@
+package pt.ulusofona.deisi.a2020.cm.g25
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_detail)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+}
