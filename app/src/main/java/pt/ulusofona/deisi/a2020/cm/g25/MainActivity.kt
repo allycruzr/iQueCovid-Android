815cf097ea3635d98186c05d7dc634a94dbc0186
Rui Moreira - 21600035 <a21600035@alunos.ulht.pt>
2021-04-06 00:56:15 +0100

    Migração de Activities para Fragments: - DashboardFragment.kt; - ContactsFragment.kt.

--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/MainActivity.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/MainActivity.kt
@@ -1,13 +1,45 @@
 package pt.ulusofona.deisi.a2020.cm.g25
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import pt.ulusofona.deisi.a2020.cm.g25.fragments.DashboardFragment
 import pt.ulusofona.deisi.a2020.cm.g25.navigation.NavigationManager
+import pt.ulusofona.deisi.a2020.cm.g25.views.ContactsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         NavigationManager.goToDashBoardFragment(supportFragmentManager)
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_dashboard -> {
+                NavigationManager.goToDashBoardFragment(supportFragmentManager)
+                return true
+            }
+            //R.id.nav_test_list -> {
+            //  NavigationManager.goToTestListFragment(supportFragmentManager)
+            //  return true
+            //}
+            //R.id.nav_extraPage -> {
+            //  NavigationManager.goToExtraPageFragment(supportFragmentManager)
+            //  return true
+            //}
+            R.id.nav_contacts -> {
+                NavigationManager.goToContactsFragment(supportFragmentManager)
+                return true
+            }
+        }
+        return false
     }
 }
--- app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/navigation/NavigationManager.kt
+++ app/src/main/java/pt/ulusofona/deisi/a2020/cm/g25/navigation/NavigationManager.kt
@@ -4,19 +4,24 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import pt.ulusofona.deisi.a2020.cm.g25.fragments.DashboardFragment
 import pt.ulusofona.deisi.a2020.cm.g25.R
+import pt.ulusofona.deisi.a2020.cm.g25.views.ContactsFragment
 
 abstract class NavigationManager {
 
     companion object {
         private fun placeFragment(fm: FragmentManager, fragment: Fragment){
             val transition = fm.beginTransaction()
-            transition.replace(R.id.frame, fragment)
+            transition.replace(R.id.main_container, fragment)
             transition.addToBackStack(null)
             transition.commit()
         }
 
         fun goToDashBoardFragment(fm: FragmentManager) {
             placeFragment(fm, DashboardFragment())
+        }
+
+        fun goToContactsFragment(fm: FragmentManager) {
+            placeFragment(fm, ContactsFragment())
         }
     }
 }
